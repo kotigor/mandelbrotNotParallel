0a4ce375baf5f9d6241c3533c150ff4d9444abc7
Igor Konstantinov <kotigor0@gmail.com>
2022-06-01 01:01:13 +0200

    first commit

---
+++ mandelbrotwidget.h
@@ -0,0 +1,46 @@
+#ifndef MANDELBROTWIDGET_H
+#define MANDELBROTWIDGET_H
+
+#include <QPixmap>
+#include <QWidget>
+#include "renderthread.h"
+
+
+//! [0]
+class MandelbrotWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MandelbrotWidget(QWidget *parent = nullptr);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;
+#if QT_CONFIG(wheelevent)
+    void wheelEvent(QWheelEvent *event) override;
+#endif
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+private slots:
+    void updatePixmap(const QImage &image, double scaleFactor);
+    void zoom(double zoomFactor);
+
+private:
+    void scroll(int deltaX, int deltaY);
+    RenderThread thread;
+    QPixmap pixmap;
+    QPoint pixmapOffset;
+    QPoint lastDragPos;
+    double centerX;
+    double centerY;
+    double pixmapScale;
+    double curScale;
+    std::chrono::steady_clock::time_point start_time, end_time;
+};
+//! [0]
+
+#endif // MANDELBROTWIDGET_H
